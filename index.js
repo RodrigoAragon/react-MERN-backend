056de4f66e78ef2f9226fb6937df53dde950ae0c
RodrigoAragon <rodrixgo18@gmail.com>
2025-02-04 09:56:14 -0300

    React app update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const path = require('path')
+
 const express = require('express');
-const { dbConnection } = require('./database/config');
 require('dotenv').config();
 const cors = require('cors')
+const { dbConnection } = require('./database/config');
 
 ///Crear el servidor de Express
 
@@ -25,6 +27,11 @@ app.use(express.json())
 ///Rutas
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
+
+
+app.use('*',(req,res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
 
 
 ///Escuchar peticiones
